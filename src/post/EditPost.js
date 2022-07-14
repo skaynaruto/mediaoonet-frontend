0b0afd1886174936752b04809f6b5adddb95a038
Suresh Kannan <91835889+skaynaruto@users.noreply.github.com>
2022-07-14 10:39:00 +0530

    Update EditPost.js

--- src/post/EditPost.js
+++ src/post/EditPost.js
@@ -52,7 +52,6 @@ class EditPost extends Component {
                     body:"",
                     title:"",
                     error:"",
-                    postImageId:'',
                     loading:false,
                     redirectToPost:true,
                     sucess:true,
